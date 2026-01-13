2de52ce461641ed85282bf89c921b9a37713fb55
TianchengLiRed <tiancl28@uci.edu>
2026-01-12 19:44:38 -0800

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,38 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+        _numSeedsLeft = _numSeeds;
     }
 
     private void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(horizontal, vertical, 0f);
+        _playerTransform.Translate(move * _speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(_numSeedsLeft > 0)
+            {
+                PlantSeed();
+                _numSeedsLeft--;
+                _numSeedsPlanted++;
+            }
+            else
+            {
+                Debug.Log("no seeds left");
+            }
+        }
+        _plantCountUI.UpdateSeeds(_numSeedsLeft,_numSeedsPlanted);
     }
 
     public void PlantSeed ()
     {
-        
+        Instantiate(
+        _plantPrefab,
+        _playerTransform.position,
+        Quaternion.identity
+    );
     }
 }
